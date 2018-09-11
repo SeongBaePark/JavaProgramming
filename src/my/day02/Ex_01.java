729824bc2c835654f149939c99aa472d52730f69
403 <403@DESKTOP-RHFPL86>
2018-09-11 16:15:39 +0900

    First Java Programming

---
+++ src/my/day02/Ex_01.java
@@ -0,0 +1,9 @@
+package my.day02;
+
+public class Ex_01 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Welcome to the Java Word!");
+	}
+}
---
+++ src/my/day02/Hello.java
@@ -0,0 +1,10 @@
+package my.day02;
+
+public class Hello 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello Java");
+	}
+}
+
